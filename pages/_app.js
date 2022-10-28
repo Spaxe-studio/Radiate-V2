4db8fd6a207be978a71036503e10be9ae61a061c
omimiCode <barnabas.oretan@gmail.com>
2022-10-28 13:40:23 +0100

    v2 switch

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,16 @@
 import Layout from "../layout";
 import "../styles/globals.css";
-import { NotificationProvider } from "web3uikit"
-import { MoralisProvider } from "react-moralis"
+import { NotificationProvider } from "web3uikit";
+import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <MoralisProvider initializeOnMount = {false}>
+    <MoralisProvider initializeOnMount={false}>
       <NotificationProvider>
-        <div className="bg-home">
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
+        <div className="bg-black">
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
         </div>
       </NotificationProvider>
     </MoralisProvider>
